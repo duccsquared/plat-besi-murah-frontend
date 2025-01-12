f69182a5f3ae2f03edffe4c94d16cb1d6a8ecacf
Mario Susanto <msus0004@student.monash.edu>
2025-01-12 20:40:28 +0700

    added login page

--- components/Header.vue
+++ components/Header.vue
@@ -1,6 +1,9 @@
 <template>
-	<div class="bg-gray-700 h-16 text-white flex flex-row items-center p-5">
+	<div class="bg-gray-700 h-16 text-white flex flex-row items-center justify-between p-5">
             <a class="text-4xl" href="/">Blogging Website (User End)</a>
+			<div class="flex flex-row space-y-2">
+				<a href="/login">Login</a>
+			</div>
 	</div>
 </template>
 <script setup>
---
+++ pages/login.vue
@@ -0,0 +1,37 @@
+<template>
+	<Header/>
+    <div class="flex min-h-screen flex-row bg-gray-100">
+        <div class="bg-gray-200 w-1/2 min-h-full">
+        </div>
+        <div class="bg-gray-300 w-1/2 min-h-full flex flex-col justify-center items-center space-y-4">
+            <h2 class="text-2xl">Login</h2>
+            <div class="flex flex-row space-x-2">
+                <p class="">Username/email:</p>
+                <input v-model="username">
+            </div>
+            <div class="flex flex-row space-x-2">
+                <p class="">Password:</p>
+                <input v-model="password">
+            </div>
+            <div class="flex flex-row space-x-2">
+                <button class="rounded-md bg-gray-500 p-2 w-fit text-white" @click="onLogin">login</button>
+                <button class="rounded-md bg-gray-500 p-2 w-fit text-white" @click="navigateTo('/')">back</button>
+            </div>
+        </div>
+    </div>
+</template>
+<script setup>
+
+const username = ref("")
+const password = ref("")
+
+const onLogin = () => {
+    if(username.value=="Mario" && password.value=="123456") {
+        console.log("logged in!")
+    }
+    else {
+        console.log("not logged in!")
+    }
+}
+
+</script>
