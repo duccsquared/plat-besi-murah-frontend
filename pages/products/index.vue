e2449ece2b8f845b56dab881b8d60145a807d44b
Mario Susanto <msus0004@student.monash.edu>
2025-01-28 12:17:23 +0700

    added stub pages for other pages on the header

---
+++ pages/products/index.vue
@@ -0,0 +1,10 @@
+<template>
+	<Header/>
+    <div class="flex min-h-screen flex-col bg-gray-100 p-5">
+        <H2>This page is still under construction</H2>
+        <p>Please come back later</p>
+    </div>
+</template>
+<script setup>
+
+</script>
