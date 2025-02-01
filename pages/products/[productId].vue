24aae7e8c54bc3a383f878668e33962bb66c3c73
Mario Susanto <msus0004@student.monash.edu>
2025-02-01 18:29:08 +0700

    modified product page to handle newlines and removed hardcoded data

--- pages/products/[productId].vue
+++ pages/products/[productId].vue
@@ -3,10 +3,7 @@
     <img :src="mainImage" class="w-full h-[12rem] sm:h-[16rem] md:h-[24rem] lg:h-[30rem] xl:h-[36rem] overflow-hidden"/>
     <div class="flex min-h-screen flex-col bg-gray-100 p-12">
         <H1 class="pb-2">{{productData?.name}}</H1>
-        <!-- <div v-for="body in bodyList">
-            <p class="pb-2">{{ body }}</p>
-        </div> -->
-        <p>{{ productData?.desc }}</p>
+        <p style="white-space: pre-line;">{{ productData?.desc }}</p>
         <div class="w-full flex flex-col items-center">
             <!-- image grid -->
             <div class="grid grid-cols-2 md:grid-cols-3 gap-6 md:gap-12 lg:gap-16 xl:gap-20 justify-center py-5">
@@ -41,23 +38,6 @@ const route = useRoute();
 const productId = route.params.productId
 const productData = ref(null)
 
-const title = "Plat Baby Coil"
-const bodyList = [
-    "Kami menyediakan plat baby coil dengan harga murah dan dapat membantu untuk menekan biaya produksi. Plat baby coil terdiri dari beberapa spesifikasi sebagai berikut:",
-    "",
-    "Baby Coil SPHC (plat Hitam) & SPHC-PO (plat abu-abu)",
-    "tebal: 1.8-6.0mm",
-    "Lebar: 900-1220mm",
-    "panjang: standard atau sesuai ",
-    "kebutuhan",
-    "",
-    "Baby Coil SPCC-SD & SPCC-UN (plat putih)",
-    "tebal: 0.4-1.8mm",
-    "Lebar: 900-1220mm",
-    "panjang: standard atau sesuai ",
-    "kebutuhan"
-]
-
 const imageList = [
   "/img/platBabyCoil1.png",
   "/img/platBabyCoil2.png",
@@ -80,6 +60,7 @@ const mainImage = "/img/platBabyCoil1.png"
 onMounted(async ()=> {
     await useApi(`/addition`,"GET",null,{num1:5,num2:5});
     const result = await useApi(`/products`,"GET",null,{id:productId});
+    console.log(result)
     if(result.isSuccess==true) {
         productData.value = result.data
     }
