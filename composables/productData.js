e380642bd2682ad62122a3d8a7edb1814404919f
Mario Susanto <msus0004@student.monash.edu>
2025-02-06 22:54:16 +0700

    added full descriptions for products

--- composables/productData.js
+++ composables/productData.js
@@ -34,26 +34,88 @@ kebutuhan
         name: "Cutting Size Plate",
         mainImage: "/img/cuttingSizePlateMain.png",
         imageList: ["/img/cuttingSizePlate1.png","/img/cuttingSizePlate2.png","/img/cuttingSizePlate3.png","/img/cuttingSizePlate4.png","/img/cuttingSizePlate5.png","/img/cuttingSizePlate6.png"],
-        desc: "Description for Cutting Size Plate"
+        desc: `
+Kami menyediakan plat besi dengan ukuran sesuai dengan kebutuhan produksi anda. Dengan didukung oleh mesin-mesin yang memadai dan team yang berpengalaman membuat kami dipercaya oleh customer untuk menyediakan plat dengan ukuran sesuai dengan kebutuhan produksi sehingga dapat mempelancar proses produksi. 
+
+HARGA MURAH
+Keunggulan utama kami adalah harga plat yang kami tawarkan lebih murah karena menggunakan baby coil. 
+
+NO WASTE
+Keunggulan lain adalah tidak ada sisa plat. Semua plat yang kami suppy dapat langsung digunakan untuk produksi sehingga perhitungan biaya produksi dapat lebih pasti.
+
+HEMAT TENAGA
+Dengan adanya cutting size plate tidak perlu tambahan biaya karyawan untuk memotong plat sebelum produksi.
+
+HEMAT WAKTU
+Waktu produksi dapat dipersingkat dengan adanya plat sesuai dengan kebutuhan produksi. 
+
+Jenis plat:
+SPHC (plat Hitam) & SPHC-PO (plat abu-abu)
+Tebal: 1.8-6.0mm
+Ukuran: Sesuai kebutuhan
+
+SPCC-SD & SPCC-UN (plat putih)
+Tebal: 0.4-1.8mm
+Ukuran: Sesuai kebutuhan
+        `
     },
     basePlate: {
         id: "basePlate",
         name: "Base Plate",
-        desc: "Description for Base Plate",
+        desc: `
+Kami menyediakan pembuatan Base Plate sesuai kebutuhan perusahaan anda dengan harga terjangkau dan berkualitas. Kami dapat membuat Base Plate dengan ukuran dan bentuknya khusus sesuai keinginan anda.
+
+Dengan mengunakan mesin laser cutting sehingga base plate yang dihasilkan menjadi sangat presisi.
+ 
+Spesifikasi base plate kami
+Jenis Plat: SPHC (plat Hitam), SPHC-PO (plat abu-abu) & SPCC-SD(plat putih)
+Tebal: 1.0-6.0mm
+Ukuran: sesuai dengan kebutuhan
+
+        `,
         mainImage: "/img/basePlateMain.png",
         imageList: ["/img/basePlate1.png","/img/basePlate2.png","/img/basePlate3.png","/img/basePlate4.png","/img/basePlate5.png","/img/basePlate6.png"],
     },
     besiSikuLubang: {
         id: "besiSikuLubang",
         name: "Besi Siku Lubang",
-        desc: "Description for Besi Siku Lubang",
+        desc: `
+Kami memproduksi dan menjual besi siku lubang, ambalan dan segitiga siku dengan kualitas bagus dan harga murah. Kami sudah memiliki pengalaman memproduksi lebih dari 5 tahun sehingga kami sudah mendapatkan kepercayaan dan menjalin kerjasama yang baik dengan banyak customer untuk mensupply produk kami baik produk jadi maupun bahan baku.
+
+Produk kami tersedia dalam beberapa ukuran dan ketebalan 
+
+Besi SIku Lubang
+Ukuran: 3.75 x 3.75 cm
+Panjang: 2 meter
+Tebal: 1.0mm, 1.2mm, 1.4mm, 1.6mm, 1.8mm, 
+            2.0mm, 2.3mm, 2.5mm 2.8mm, 3.0mm
+ 
+Ambalan 
+Bahan: SPCC-SD dan SPCC-UN
+Ukuran: 40x100cm (50x110cm ukuran bahan)
+Tebal: 0.6, 0.7, 0.8mm
+
+Segitiga Siku Lubang
+Bahan: SPCC-SD dan SPCC-UN
+Ukuran: 7.8x7.8cm 
+Tebal: 0.6-0.8mm
+        `,
         mainImage: "/img/besiSikuLubangMain.png",
         imageList: ["/img/besiSikuLubang1.png","/img/besiSikuLubang2.png","/img/besiSikuLubang3.png","/img/besiSikuLubang4.png","/img/besiSikuLubang5.png","/img/besiSikuLubang6.png"],
     },
     platJackBaseUHead: {
         id: "platJackBaseUHead",
         name: "Plat Jack Base & U-head",
-        desc: "Description for Plat U-head",
+        desc: `
+Kami menyediakan plat jack base dan plat U-head untuk kebutuhan produksi Jack Base & U-Head scaffolding dengan harga terjangkau dan berkualitas. Plat jack base dan u-head kami memiliki kualitas yang bagus karena kamu menggunakan mesin laser cutting sehingga memiliki  tingkat presisi sempurna.
+
+Spesifikasi:
+Jenis plat: SPHC dan SPHC-PO
+Tebal: 3.9-4.0mm
+Ukuran: 130 x 130mm(jackbase)
+               130 x 130mm(130x190mm U-head)
+
+        `,
         mainImage: "/img/platJackBaseUHeadMain.png",
         imageList: ["/img/platJackBaseUHead1.png","/img/platJackBaseUHead2.png","/img/platJackBaseUHead3.png","/img/platJackBaseUHead4.png","/img/platJackBaseUHead5.png","/img/platJackBaseUHead6.png"],
     },
