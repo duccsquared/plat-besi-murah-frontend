bdde98fa42638a6e281257918803ca04b56378d0
Mario Susanto <msus0004@student.monash.edu>
2025-02-06 22:45:28 +0700

    added images for products

--- composables/productData.js
+++ composables/productData.js
@@ -1,15 +1,17 @@
 export const useProductData = () => {
-    const productData = useState("productData", () => useCookie("productData"));
-    if(productData.value==null) {
-        productData.value = productDetails
-    }
+    // const productData = useState("productData", () => useCookie("productData"));
+    // if(productData.value==null) {
+    //     productData.value = productDetails
+    // }
     return {productData}
 }
 
-const productDetails = {
+const productData = {
     platBabyCoil: {
         id: "platBabyCoil",
         name: "Plat Baby Coil",
+        mainImage: "/img/platBabyCoilMain.png",
+        imageList: ["/img/platBabyCoil1.png","/img/platBabyCoil2.png","/img/platBabyCoil3.png","/img/platBabyCoil4.png","/img/platBabyCoil5.png","/img/platBabyCoil6.png"],
         desc: `
 Kami menyediakan plat baby coil dengan harga murah dan dapat membantu untuk menekan biaya produksi. Plat baby coil terdiri dari beberapa spesifikasi sebagai berikut:
 
@@ -30,26 +32,36 @@ kebutuhan
     cuttingSizePlate: {
         id: "cuttingSizePlate",
         name: "Cutting Size Plate",
+        mainImage: "/img/cuttingSizePlateMain.png",
+        imageList: ["/img/cuttingSizePlate1.png","/img/cuttingSizePlate2.png","/img/cuttingSizePlate3.png","/img/cuttingSizePlate4.png","/img/cuttingSizePlate5.png","/img/cuttingSizePlate6.png"],
         desc: "Description for Cutting Size Plate"
     },
     basePlate: {
         id: "basePlate",
         name: "Base Plate",
-        desc: "Description for Base Plate"
+        desc: "Description for Base Plate",
+        mainImage: "/img/basePlateMain.png",
+        imageList: ["/img/basePlate1.png","/img/basePlate2.png","/img/basePlate3.png","/img/basePlate4.png","/img/basePlate5.png","/img/basePlate6.png"],
     },
     besiSikuLubang: {
         id: "besiSikuLubang",
         name: "Besi Siku Lubang",
-        desc: "Description for Besi Siku Lubang"
+        desc: "Description for Besi Siku Lubang",
+        mainImage: "/img/besiSikuLubangMain.png",
+        imageList: ["/img/besiSikuLubang1.png","/img/besiSikuLubang2.png","/img/besiSikuLubang3.png","/img/besiSikuLubang4.png","/img/besiSikuLubang5.png","/img/besiSikuLubang6.png"],
     },
     platJackBaseUHead: {
         id: "platJackBaseUHead",
         name: "Plat Jack Base & U-head",
-        desc: "Description for Plat U-head"
+        desc: "Description for Plat U-head",
+        mainImage: "/img/platJackBaseUHeadMain.png",
+        imageList: ["/img/platJackBaseUHead1.png","/img/platJackBaseUHead2.png","/img/platJackBaseUHead3.png","/img/platJackBaseUHead4.png","/img/platJackBaseUHead5.png","/img/platJackBaseUHead6.png"],
     },
     bracket: {
         id: "bracket",
         name: "Bracket",
-        desc: "Description for Bracket"
+        desc: "Description for Bracket",
+        mainImage: "/img/cuttingSizePlateMain.png",
+        imageList: ["/img/cuttingSizePlate1.png","/img/cuttingSizePlate2.png","/img/cuttingSizePlate3.png","/img/cuttingSizePlate4.png","/img/cuttingSizePlate5.png","/img/cuttingSizePlate6.png"],
     },
 }
--- pages/products/[productId].vue
+++ pages/products/[productId].vue
@@ -1,13 +1,13 @@
 <template>
 	<Header/>
-    <img :src="mainImage" class="w-full h-[12rem] sm:h-[16rem] md:h-[24rem] lg:h-[30rem] xl:h-[36rem] overflow-hidden"/>
+    <img :src="productData?.mainImage" class="w-full h-[12rem] sm:h-[16rem] md:h-[24rem] lg:h-[30rem] xl:h-[36rem] overflow-hidden"/>
     <div class="flex min-h-screen flex-col bg-gray-100 p-12">
         <H1 class="pb-2">{{productData?.name}}</H1>
         <p style="white-space: pre-line;">{{ productData?.desc }}</p>
         <div class="w-full flex flex-col items-center">
             <!-- image grid -->
             <div class="grid grid-cols-2 md:grid-cols-3 gap-6 md:gap-12 lg:gap-16 xl:gap-20 justify-center py-5">
-                <div v-for="image in imageList" :key="image" class="cursor-pointer" @click="openModal(image)">
+                <div v-for="image in productData?.imageList" :key="image" class="cursor-pointer" @click="openModal(image)">
                     <img :src="image" class="w-full h-auto rounded-md shadow-md hover:scale-105 transition duration-200" />
                 </div>
             </div>
@@ -60,8 +60,7 @@ const openModal = (image) => {
   selectedImage.value = image;
 };
 
-const mainImage = "/img/platBabyCoil1.png"
-productData.value = useProductData().productData.value[productId]
+productData.value = useProductData().productData[productId]
 
 // onMounted(async ()=> {
 //     await useApi(`/addition`,"GET",null,{num1:5,num2:5});
