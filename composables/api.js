8723128f323d2c7c762ad47f274bc09a0ea246be
Mario Susanto <msus0004@student.monash.edu>
2025-02-20 22:50:17 +0700

    test change

--- composables/api.js
+++ composables/api.js
@@ -2,7 +2,7 @@ export const useApi = async (request, method,body=null,query=null) => {
   const config = useRuntimeConfig(); 
   console.log("API BASE URL:", config.public.apiBase); // Debugging
   let result = await useLazyFetch(formatRequest(request, query), {
-      baseURL: config.public.apiBase,
+      baseURL: process.env.NUXT_PUBLIC_API_BASE, // config.public.apiBase
         method: method,
         body: method != "GET" & body!=null ? JSON.stringify(body) : null,
         headers: {
