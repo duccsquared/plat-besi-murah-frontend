2bfee97d76978f8366ffd29f668180c36540b8f4
Mario Susanto <msus0004@student.monash.edu>
2025-02-21 16:08:01 +0700

    altered fallback API

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,7 +15,7 @@ export default defineNuxtConfig({
   ],
   runtimeConfig: {
     public: {
-      apiBase: process.env.NUXT_API_BASE_URL || 'http://localhost:4321/api/', // Fallback to localhost
+      apiBase: process.env.NUXT_API_BASE_URL || 'http://plat-besi-murah-backend-env-1.eba-r2ntmjag.us-east-2.elasticbeanstalk.com/api/', // Fallback to localhost
     },
   },
 
