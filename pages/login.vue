ecaa529020e293cd97aa96de676e5bf881ce6367
duccsquared <mariosusanto161@gmail.com>
2025-10-07 00:57:32 +0700

    updated login screen styling

--- pages/login.vue
+++ pages/login.vue
@@ -1,41 +1,31 @@
 <template>
-    <div class="flex flex-row">
-        <div class="bg-gray-200 w-1/2 min-h-full">
-        </div>
-        <div class="bg-gray-300 w-1/2 min-h-full flex flex-col justify-center items-center space-y-4">
-            <H2 class="text-2xl">Login</H2>
-            <div class="flex flex-row space-x-2">
-                <p>Username/email:</p>
-                <input v-model="username">
-            </div>
-            <div class="flex flex-row space-x-2">
-                <p>Password:</p>
-                <input v-model="password">
-            </div>
-            <div class="flex flex-row space-x-2">
-                <button class="rounded-md bg-gray-500 p-2 w-fit text-white" @click="onLogin">login</button>
-                <button class="rounded-md bg-gray-500 p-2 w-fit text-white" @click="navigateTo('/')">back</button>
-            </div>
-        </div>
+  <div class="flex flex-col min-h-screen items-center justify-center bg-stone-100 dark:bg-gray-800">
+    <div class="min-w-2/5 w-2/5 bg-white rounded-lg shadow-lg flex flex-col space-y-4 p-4">
+      <h1 class="text-2xl text-bold">Plat Besi Murah Admin Login</h1>
+      <div class="flex flex-rol justify-between">
+        <p>Username</p>
+        <input v-model="username" class="border border-gray-900 rounded-lg"/>
+      </div>
+      <div class="flex flex-rol justify-between">
+        <p>Password</p>
+        <input type="password" v-model="password" class="border border-gray-900 rounded-lg"/>
+      </div>
+      <Button @click="login">Login</Button>
     </div>
+  </div>
 </template>
-<script setup>
 
+<script setup>
 const username = ref("")
 const password = ref("")
 
-const onLogin = () => {
-    if(username.value=="Mario" && password.value=="123456") {
-        console.log("logged in!")
-        useToast().success("login successful!")
-        useUserInfo().username.value = "Mario"
-        useUserInfo().password.value = "123456"
-        navigateTo("/")
-    }
-    else {
-        console.log("not logged in!")
-        useToast().error("login failed!")
-    }
+const login = () => {
+  if(username.value=="admin" && password.value=="admin") {
+    useNotification().showSuccess("Login succeeded!")
+    navigateTo("/")
+  }
+  else {
+    useNotification().showError("Login failed; incorrect username or password")
+  }
 }
-
 </script>
